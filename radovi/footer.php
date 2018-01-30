3ca4a229c608b16ea9f10485e8dc3acdfefa7571
Jelena <mijanovic_jelena@yahoo.com>
2018-01-30 20:48:00 +0100

    Added new file

--- radovi/footer.php
+++ radovi/footer.php
@@ -1,2 +1,10 @@
-</body>
-</html>
+
+
+
+<div class="date-in-header">
+    Ponedjeljak, Januar 29, 2018   </div>
+
+    <div class="social-links clearfix">
+  <ul>
+  <li><a href="https://www.facebook.com/istinitotrebinje/" ><i class="fa fa-facebook"></i></a></li>		</ul>
+  </div><!-- .social-links -->
